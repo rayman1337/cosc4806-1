94de00b4fe3055203737cefe40c64d705a1a3336
RM <134272733+rayman1337@users.noreply.github.com>
2025-05-23 22:48:40 +0000

    Setting up initial project structure

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
+<!DOCTYPE html>
 <html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+<body>
 
+<h2>Login with your username and password</h2>
+
+<form action="/verify.php">
+  <label for="username">Username:</label><br>
+  <input type="text" id="username" name="username"><br>
+  <label for="password">Password:</label><br>
+  <input type="text" id="password" name="password"><br>
+  <input type="submit" value="Login">
+</form> 
+</body>
 </html>
+
